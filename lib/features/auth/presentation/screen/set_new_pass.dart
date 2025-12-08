08118103c0ed147c602f8d43640567b40f402dcc
Musfikur Rahman <musfikr679@gmail.com>
2025-12-08 15:42:05 +0600

    add or field to sign in and sign up

---
+++ lib/features/auth/presentation/screen/set_new_pass.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SetNewPass extends StatelessWidget {
+  const SetNewPass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
